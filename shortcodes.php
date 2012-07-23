c4abd9f707e9eaec748b96751657a615c7c627be
Patrick Proctor <patrick42h@gmail.com>
2012-07-23 16:15:22 -0400

    Added IUShortcodes class and a couple action hooks for testing purposes.

--- shortcodes.php
+++ shortcodes.php
@@ -25,18 +25,31 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-add_action();
+add_action('wp_head', function() { echo 'shortcodes, yeah!'; });
+add_action('loop_start', array('IUShortcodes', 'generate_shortcode'));
+// add_shortcode("googlemap", "fn_googleMaps");
 
-// Google Maps Shortcode
-function fn_googleMaps($atts, $content = null) {
-	extract(shortcode_atts(array(
-		"width" => '425',
-		"height" => '550',
-		"src" => ''
-	), $atts));
-	
-	return ($src) ? '<iframe width="'.$width.'" height="'.$height.'" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="'.$src.'&amp;output=embed" class="googlemap"></iframe><small class="googlemaplink"><a href="'. $src .'" target="_blank" title="View a larger version of this map">View larger map</a></small>' : false;
+if ( !class_exists('IUShortcodes') ) {
+	/**
+	* IU Shortcodes
+	*/
+	class IUShortcodes {
+		$width = 550;
+		$height = 315;
+		$embed_codes = array(
+			'<iframe width="'.$width.'" height="'.$height.'" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="'.$src.'&amp;output=embed" class="googlemap"></iframe><small class="googlemaplink"><a href="'. $src .'" target="_blank" title="View a larger version of this map">View larger map</a></small>', // Google Maps
+			'<iframe src="http://vimeo.com/36684976" width="" height="" frameborder="0"></iframe>'
+		);
+		
+		function generate_shortcode($atts, $content = null) {
+			extract(shortcode_atts(array(
+				"width" => '425',
+				"height" => '550',
+				"src" => ''
+			), $atts));
+
+			return ($src) ? '<iframe width="'.$width.'" height="'.$height.'" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="'.$src.'&amp;output=embed" class="googlemap"></iframe><small class="googlemaplink"><a href="'. $src .'" target="_blank" title="View a larger version of this map">View larger map</a></small>' : false;
+		}
+	}
 }
-add_shortcode("googlemap", "fn_googleMaps");
-
 ?>
