ff99ff502a9f2a6af5ea7f1c118b45e9d907bcee
Patrick Proctor <patrick42h@gmail.com>
2012-07-23 18:31:29 -0400

    Added shortcode functions for YouTube, Vimeo, and Google Maps.

--- shortcodes.php
+++ shortcodes.php
@@ -25,15 +25,49 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
+$global_width = '550';
+$global_height = '315';
+$global_src = '';
+$embed_services = array('googlemaps','vimeo','youtube');
+
 function vimeo_shortcode($atts) {
+	global $global_width;
+	global $global_height;
 	extract(shortcode_atts(array(
-		"width" => '550',
-		"height" => '315',
+		"width" => $global_width,
+		"height" => $global_height,
 		"src" => ''
 	), $atts));
-
-	return '<iframe src="http://player.vimeo.com/video/'.$src.'?color=ffffff" width="'.$width.'" height="'.$height.'" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>';
-	// return '<iframe src="'.$src.'" width="'.$width.'" height="'.$height.'" frameborder="0"></iframe>';
+	return ($src) ? '<iframe src="http://player.vimeo.com/video/'.$src.'" width="'.$width.'" height="'.$height.'" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>' : false;
 }
-add_shortcode('vimeo', 'vimeo_shortcode');
+function googlemaps_shortcode($atts) {
+	global $global_width;
+	global $global_height;
+	extract(shortcode_atts(array(
+		"width" => $global_width,
+		"height" => $global_height,
+		"src" => ''
+	), $atts));
+	return ($src) ? '<iframe width="'.$width.'" height="'.$height.'" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="'.$src.'&output=embed" class="googlemap"></iframe><small class="googlemaplink"><a href="'. $src .'" target="_blank" title="View a larger version of this map">View larger map</a></small>' : false;
+}
+function youtube_shortcode($atts) {
+	global $global_width;
+	global $global_height;
+	extract(shortcode_atts(array(
+		"width" => $global_width,
+		"height" => $global_height,
+		"src" => ''
+	), $atts));
+	return ($src) ? '<iframe width="'.$width.'" height="'.$height.'" src="http://www.youtube.com/embed/'.$src.'" frameborder="0" allowfullscreen></iframe>' : false;
+}
+
+function print_services() {
+	global $embed_services;
+	foreach ($embed_services as $embed_service) {
+		$function_name = $embed_service.'_shortcode';
+		if (function_exists($function_name)) { add_shortcode($embed_service, $function_name); }
+	}
+}
+
+add_action('loop_start', 'print_services');
 ?>
